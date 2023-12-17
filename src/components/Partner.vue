7c843b6a2de8866945b668e697ff396996a29d02
farattiana <faratianarazafindrabe@gmail.com>
2023-12-17 19:32:21 +0300

    feat: about

--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="overflow-hidden bg-champagne">
-    <div class=" bg-champagne flex flex-row gap-5 m-4 sm:m-36 max-h-32 xl:justify-center items-center border-solid border-y-2 overflow-x-auto">
+    <div class=" bg-champagne flex flex-row gap-5 my-10 mx-14 sm:m-36 max-h-32 xl:justify-center items-center border-solid border-y-2 overflow-x-auto">
       <h1>Supported by: </h1>
       <img src="/spotify.png" alt="">
       <img src="/google.png" alt="">
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -8,7 +8,15 @@ const imagesAbout = [
 </script>
 
 <template>
-  <div class="flex flex-row justify-center p-4 gap-4  flex-wrap sm:px-36">
-    <IllustrationAbout v-for="image in imagesAbout" :image="image"/>
+  <div class="relative">
+    <div class="flex justify-center items-center">
+      <img src="/scribbleAbout.svg" alt="" class="absolute">
+    </div>
+    <div class="flex flex-col justify-center items-center gap-10 py-40">
+      <h1 class="text-4xl sm:text-6xl text-center max-w-3xl font-bold">Talk. Listen. Get inspired by every minute of it.</h1>
+      <div class="flex flex-row md:flex-nowrap justify-center p-4 gap-4  flex-wrap sm:px-36">
+        <IllustrationAbout v-for="image in imagesAbout" :image="image"/>
+      </div>
+    </div>
   </div>
 </template>
