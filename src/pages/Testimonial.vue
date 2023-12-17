e5448eda2069b7ce77a02735bea85e51ca1c7a80
farattiana <faratianarazafindrabe@gmail.com>
2023-12-17 20:57:30 +0300

    feat: testimonial section

---
+++ src/pages/Testimonial.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="relative">
+    <div class="flex justify-center items-center">
+      <img src="/scribble.svg" alt="" class="absolute">
+    </div>
+    <div class="bg-white flex justify-center items-center p-20 sm:pt-40">
+      <div class="sm:pt-20 flex flex-row relative">
+        <img src="/sparkle.svg" alt="" class="absolute top-[-50px] left-[-50px] sm:top-0 sm:left-[-100px]">
+        <div class="flex flex-col text-center gap-2">
+          <h1 class="text-4xl text-center sm:text-6xl">What our listeners say</h1>
+          <p class="text-gray">Their experience throughout every platform</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
