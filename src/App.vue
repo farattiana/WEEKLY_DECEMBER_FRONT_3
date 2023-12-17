f8c1155d29cc10441778f94b651edcb2cd9a9ada
Fuji-Noobie-Ghost <fitahiana.ratia@gmail.com>
2023-12-17 22:56:45 +0300

    feat: Testimonial Card

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import Cover from './components/Cover.vue';
 import Partner from './components/Partner.vue';
 import About from './pages/About.vue';
 import Testimonial from './pages/Testimonial.vue';
+import TestimonialCard from './components/TestimonialCard.vue';
 </script>
 
 <template>
