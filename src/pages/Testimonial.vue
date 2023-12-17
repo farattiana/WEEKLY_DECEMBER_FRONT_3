96d8d1522cbcb4fcbff3bbfb00279f42f8123a12
farattiana <faratianarazafindrabe@gmail.com>
2023-12-17 21:08:00 +0300

    modified: font bold

--- src/pages/Testimonial.vue
+++ src/pages/Testimonial.vue
@@ -10,7 +10,7 @@
       <div class="sm:pt-20 flex flex-row relative">
         <img src="/sparkle.svg" alt="" class="absolute top-[-50px] left-[-50px] sm:top-0 sm:left-[-100px]">
         <div class="flex flex-col text-center gap-2">
-          <h1 class="text-4xl text-center sm:text-6xl">What our listeners say</h1>
+          <h1 class="text-4xl text-center font-bold sm:text-6xl">What our listeners say</h1>
           <p class="text-gray">Their experience throughout every platform</p>
         </div>
       </div>
