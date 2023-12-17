65ba9a18374925108f24c04e63c6c6aaa4841209
farattiana <faratianarazafindrabe@gmail.com>
2023-12-17 22:03:08 +0300

    feat: feature section

--- src/components/ButtonSecondary.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-</script>
-
-<template>
-  <button class="bg-transparent border-2 border-amber text-blue font-cardo px-4 py-2 sm:px-8 sm:py-3 w-max">
-    Order Today
-  </button>
-</template>
