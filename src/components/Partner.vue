76b0169a807072cb34a2d5c7b3e0fb5163e7307e
farattiana <faratianarazafindrabe@gmail.com>
2023-12-17 20:31:38 +0300

    modified: scroll partner

--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="overflow-hidden bg-champagne">
-    <div class=" bg-champagne flex flex-row sm:m-36 max-h-32 xl:justify-between items-center border-solid border-y-2 overflow-x-auto">
+    <div class=" bg-champagne flex flex-row sm:m-36 max-h-32 xl:justify-between items-center border-solid border-y-2 overflow-x-auto overflow-y-hidden">
       <h1 class="font-bold">Supported by: </h1>
       <div class="flex">
         <img class="py-5 px-16" src="/spotify.png" alt="">
