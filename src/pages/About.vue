66f1834a5a8aec9364bccbcd92ce7e8e5e110cf2
farattiana <faratianarazafindrabe@gmail.com>
2023-12-17 21:08:00 +0300

    modified: font bold

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -13,14 +13,14 @@ const imagesAbout = [
       <img src="/scribbleAbout.svg" alt="" class="absolute">
     </div>
     <div class="flex flex-col justify-center items-center gap-10 pt-40">
-      <h1 class="text-4xl sm:text-6xl text-center max-w-3xl font-bold">Talk. Listen. Get inspired by every minute of it.</h1>
+      <h1 class="text-4xl sm:text-6xl text-center max-w-4xl font-bold">Talk. Listen. Get inspired by every minute of it.</h1>
       <div class="flex flex-row md:flex-nowrap justify-center p-4 gap-4  flex-wrap sm:px-36">
         <IllustrationAbout v-for="image in imagesAbout" :image="image"/>
       </div>
     </div>
     <div class="flex flex-col justify-center items-center bg-champagne md:m-44 p-11 gap-10">
       <span class="text-8xl text-center font-bold text-red">“</span>
-      <p class="font-bold text-4xl text-center max-w-3xl">One of the best daily podcasts that covers every topic on Spotify.</p>
+      <p class="font-bold text-3xl sm:text-4xl text-center max-w-3xl">One of the best daily podcasts that covers every topic on Spotify.</p>
       <div class="flex flex-col sm:flex-row justify-center items-center gap-3">
         <div class="flex flex-row items-center font-bold gap-3">
           <img src="/avatar.png" alt="" class="rounded-[50%]">
